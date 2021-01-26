f684cba6fe4156b1aab47b5871285351e27cbf63
Arturo Galan <arturo.galan@clarity.ai>
2021-01-26 12:43:36 +0100

    plugin installed

---
+++ content/blog/test.md
@@ -0,0 +1,8 @@
+---
+title: test
+excerpt: test
+date: 2021-01-25T19:26:18.162Z
+---
+# Should I start to learn Cobol?
+
+The new cute edge language Cobol is object oriented.
--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,4 @@
-import CMS from "netlify-cms"
+import CMS from 'netlify-cms'
+import NetlifyCmsWidgetI18n from 'netlify-cms-widget-i18n'
+
+CMS.registerWidget('i18n', I18nControl, I18nPreview)
